fe34f2a83a043fb1344a1ada8d66150a01ec8560
Vermzzyy <cpt.woof16@gmail.com>
2026-05-18 10:54:48 +0800

    services

--- src/features/services/ServicesPage.tsx
+++ src/features/services/ServicesPage.tsx
@@ -15,7 +15,7 @@ export interface Service {
   thumbnail: string;
 }
 
-const CATEGORIES = ['All', 'UI/UX', 'Graphic Design', 'Game Development', '3D & Animation', 'Web Development', 'Scripting', 'Building'];
+const CATEGORIES = ['All', 'UI/UX', 'Graphic Design', '3D & Animation', 'Web Development', 'Scripting', 'Building'];
 
 export default function ServicesPage() {
   const [search, setSearch] = useState('');
